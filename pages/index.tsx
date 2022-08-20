642218be9f5cd3a29bf0ed0cb45c7e23069a2e28
Ehsan Sumra <ehsansumra@gmail.com>
2022-08-20 12:56:56 -0400

    refresh optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link';
 import HeaderBox from '../components/headerbox';
 import LinkBox from '../components/linkbox';
 import MainHeader from '../components/mainheader';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Main: NextPage = () => {
+    const { pathname } = useRouter();
+    useEffect(() => {
+        // some browsers (like safari) may require a timeout to delay calling this
+        // function after a page has loaded; otherwise, it may not update the position
+        window.scrollTo(0, 0);
+      }, [pathname]);
     return (
         <>
 
