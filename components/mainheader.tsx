bb9277a97f5845b12af7d21bf3cfe667b170b337
Ehsan Sumra <ehsansumra@gmail.com>
2022-08-20 11:33:28 -0400

    more mobile ui

--- components/mainheader.tsx
+++ components/mainheader.tsx
@@ -4,11 +4,13 @@ interface Props {
     children: React.ReactNode;
     style?: React.CSSProperties;
     margin?: boolean;
+    firstHeader?: boolean;
 }
 
-const MainHeader: FC<Props> = ({ children, style, margin }) => {
+const MainHeader: FC<Props> = ({ children, style, margin, firstHeader}) => {
     let marginStyle = " sm:mt-0 mt-48"
     if (margin) marginStyle = " sm:mt-10 mt-48";
+    if (firstHeader) marginStyle = " sm:mt-0 mt-36"
     let defaultStyle = "text-2xl text-violet-300" + marginStyle;
     
     return (
