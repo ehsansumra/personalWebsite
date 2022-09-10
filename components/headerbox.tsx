8dcc3fdc72511a7aff69cff5d423b7597bea9649
Ehsan Sumra <ehsansumra@gmail.com>
2022-09-09 22:02:16 -0400

    bruh

--- components/headerbox.tsx
+++ components/headerbox.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const HeaderBox: FC<Props> = ({ children, style }) => {
     return (
-        <h2 style={style} className="mt-4 text-lg border inline-block text-violet-200 border-violet-300 p-1 px-2">
+        <h2 style={style} className="mt-4 text-xl  inline-block text-green-300 border-violet-300 p-1 px-2">
             {children}
         </h2>
 
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Transition from '../components/transition';
+import Image from 'next/image';
+import resume from '../public/resume.png';
 const Resume: NextPage = () => {
     const { pathname } = useRouter();
     useEffect(() => {
@@ -18,7 +20,7 @@ const Resume: NextPage = () => {
                 <div className=" pattern w-full bg-gray-900 min-h-screen pt-12 flex flex-wrap justify-center">
                     <section className=" bg-gray-900  shadow-gray-900 flex flex-col min-h-full w-navWidth p-8 px-5 mt-30  md:mx-2">
 
-                        <img src="resume.png" />
+                        <Image layout="responsive" src={resume}/>
 
 
 
